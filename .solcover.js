f1fbb87a481cecc9ed273c611969f1fdcba52a58
Cooller458 <omrelibol4@gmail.com>
2024-01-03 14:05:19 +0300

    balanceSwapRouter

---
+++ .solcover.js
@@ -0,0 +1,16 @@
+module.exports = {
+  skipFiles: [
+    "libraries/Babylonian.sol",
+    "libraries/Math.sol",
+    "libraries/PancakeLibrary.sol",
+    "libraries/SafeMath.sol",
+    "libraries/UQ112x112.sol",
+    "libraries/WBNB.sol",
+    "BalanceSwapERC20.sol",
+    "BalanceSwapFactory.sol",
+    "BalanceSwapPair.sol",
+    "BalanceSwapRouter.sol",
+    "BalanceSwapRouter01.sol",
+    "utils/MockERC20.sol",
+  ],
+};
